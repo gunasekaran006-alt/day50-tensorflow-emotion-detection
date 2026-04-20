adb1ea1f385d3fd47700b6c81e3d5634316d694a
gunasekaran006-alt <gunasekaran006@gmail.com>
2026-04-20 23:50:03 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import React, { useEffect, useRef, useState } from 'react'
+import * as faceapi from "face-api.js";
+
+
+/*
+1 Camera Access and start the camera
+2 Model loading
+- 3 Detect the emotion from camera video
+- 4 Output
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const videoRef = useRef();
+  const [emotion, setEmotion] = useState("Loading...");
+
+  useEffect(() => {
+    async function start() {
+      const model_url = "/models";
+
+      // Load Models
+      await faceapi.nets.tinyFaceDetector.loadFromUri(model_url);
+      await faceapi.nets.faceExpressionNet.loadFromUri(model_url);
+
+      console.log("Model Loaded");
+
+      // Start Camera
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true
+      });
+
+      videoRef.current.srcObject = stream;
+      setEmotion("Detecting...")
+
+    }
+
+    start();
+  }, []);
+
+  useEffect(() => {
+
+    const interval = setInterval(async () => {
+
+      if (!videoRef.current) return;
+
+      // run the model
+      const detections = await faceapi.detectAllFaces(videoRef.current,
+        new faceapi.TinyFaceDetectorOptions()
+      ).withFaceExpressions();
+
+      console.log(detections);
+
+      if(detections.length > 0){
+        const expressions = detections[0].expressions;
+        
+        // MAXIMUM EMOTION FINDING LOGIC
+        const maxEmotion = Object.keys(expressions).reduce((a, b) => expressions[a] > expressions[b] ? a : b);
+
+        setEmotion(maxEmotion);
+      }
+
+    }, 1000);
+
+    return () => clearInterval(interval);
+
+  }, [])
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>Emotion Detection App</h1>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      <video ref={videoRef} autoPlay muted width="600" height="450"></video>
+      <h2>Current Emotion: {emotion}</h2>
+    </div>
   )
 }
 
